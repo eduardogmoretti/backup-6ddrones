6ebd06cb9f82b323c30f52e73b4b7fbdfe01cdd2
EDUARDO GARCIA MORETTI <21.00596-6@maua.br>
2023-10-27 12:42:59 -0300

    GrupoBD

--- projeto_django/app_django/urls.py
+++ projeto_django/app_django/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from app_django.views import CadastroNovoUsuarioView
+from app_django.views import CadastroNovoGrupoView
 
 urlpatterns = [
     path('signup/', CadastroNovoUsuarioView.as_view(), name='signup'),
+    path('group/', CadastroNovoGrupoView.as_view(), name='group'),
 ]
--- projeto_django/app_django/views/__init__.py
+++ projeto_django/app_django/views/__init__.py
@@ -1,1 +1,2 @@
 from .usuario_views import CadastroNovoUsuarioView
+from .group_views import CadastroNovoGrupoView
